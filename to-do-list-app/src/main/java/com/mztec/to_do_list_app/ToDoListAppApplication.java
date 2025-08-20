552114f97deac45af1723f91a2ebfb9508593027
Andre Muniz <engenheiro.muniz@outlook.com>
2025-08-20 17:00:25 -0300

    The project start here, this is the first commit.

---
+++ to-do-list-app/src/main/java/com/mztec/to_do_list_app/ToDoListAppApplication.java
@@ -0,0 +1,13 @@
+package com.mztec.to_do_list_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListAppApplication.class, args);
+	}
+
+}
---
+++ to-do-list-app/src/test/java/com/mztec/to_do_list_app/ToDoListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mztec.to_do_list_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
